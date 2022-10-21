a74349ea275e2991807e9fbccf48ae6f60762663
dpcongdanh <dpcongdanh@gmail.com>
2022-10-21 15:58:46 +0700

    Google API Events Data displayed in the FullCalendar UI

--- src/pages/calendar/list.tsx
+++ src/pages/calendar/list.tsx
@@ -4,15 +4,12 @@ import {
   // useMany,
   useList,
 } from "@pankod/refine-core";
-import { gapi } from "gapi-script";
-// import { Calendar, momentLocalizer } from "react-big-calendar";
-// import { Calendar } from '@fullcalendar/core';
+// import { gapi } from "gapi-script";
 import FullCalendar from "@fullcalendar/react";
 import dayGridPlugin from "@fullcalendar/daygrid";
 import timeGridPlugin from "@fullcalendar/timegrid";
 import listPlugin from "@fullcalendar/list";
-// import moment from "moment";
-import "react-big-calendar/lib/css/react-big-calendar.css";
+import googleCalendarPlugin from "@fullcalendar/google-calendar";
 import {
   // useDataGrid,
   // DataGrid,
@@ -32,84 +29,70 @@ const calendarID = process.env.REACT_APP_CALENDAR_ID;
 const apiKey = process.env.REACT_APP_GOOGLE_API_KEY;
 const accessToken = process.env.REACT_APP_GOOGLE_ACCESS_TOKEN;
 
-// const localizer = momentLocalizer(moment);
-
 export const CalendarList: React.FC = () => {
-  const [events, setEvents]: any = useState([]);
+  // const [events, setEvents]: any = useState([]);
 
-  const getEvents = (calendarID?: string, apiKey?: string) => {
-    function initiate() {
-      gapi.client
-        .init({
-          apiKey: apiKey,
-        })
-        .then(function () {
-          return gapi.client.request({
-            path: `https://www.googleapis.com/calendar/v3/calendars/${calendarID}`,
-            headers: {
-              "Content-type": "application/json",
-              Authorization: `Bearer ${accessToken}`,
-            },
-          });
-        })
+  // const getEvents = (calendarID?: string, apiKey?: string) => {
+  //   function initiate() {
+  //     gapi.client
+  //       .init({
+  //         apiKey: apiKey,
+  //       })
+  //       .then(function () {
+  //         return gapi.client.request({
+  //           path: `https://www.googleapis.com/calendar/v3/calendars/${calendarID}/events`,
+  //           // headers: {
+  //           //   "Content-type": "application/json",
+  //           //   Authorization: `Bearer ${accessToken}`,
+  //           // },
+  //         });
+  //       })
 
-        .then(
-          (response: any) => {
-            let events = response.result.items;
-            return events;
-          },
-          function (err: any) {
-            return [false, err];
-          }
-        );
-    }
+  //       .then(
+  //         (response: any) => {
+  //           let events = response.result.items;
+  //           return events;
+  //         },
+  //         function (err: any) {
+  //           return [false, err];
+  //         }
+  //       );
+  //   }
 
-    gapi.load("client", initiate);
-  };
+  //   gapi.load("client", initiate);
+  // };
 
-  useEffect(() => {
-    const events = getEvents(calendarID, apiKey);
-    setEvents(events);
-  }, []);
+  // useEffect(() => {
+  //   const events = getEvents(calendarID, apiKey);
+  //   setEvents(events);
+  // }, []);
 
   return (
-    <Box sx={{ flexGrow: 1 }}>
-      <iframe
-        title="Google Calendar"
-        src="https://calendar.google.com/calendar/embed?src=0969543c2df32452ccd6b77776d1cd4aa17afc2234a0cd1a12f2477f41950d69%40group.calendar.google.com&ctz=Asia%2FHo_Chi_Minh"
-        style={{ border: 0 }}
-        width={800}
-        height={600}
-        frameBorder={0}
-        scrolling="no"
-      ></iframe>
-      {/* <Calendar
-        localizer={localizer}
-        // events={myEventsList}
-        startAccessor="start"
-        endAccessor="end"
-        style={{ height: 500 }}
-      /> */}
+    <List contentProps={{ sx: { height: "calc(100vh - 192px)" } }}>
       <FullCalendar
-        // plugins={[dayGridPlugin]}
-        // initialView="dayGridMonth"
-        plugins={[dayGridPlugin, timeGridPlugin, listPlugin]}
+        plugins={[
+          dayGridPlugin,
+          timeGridPlugin,
+          listPlugin,
+          googleCalendarPlugin,
+        ]}
+        googleCalendarApiKey={apiKey}
         initialView="dayGridMonth"
+        height={"100%"}
         headerToolbar={{
           left: "prev,next today",
           center: "title",
           right: "dayGridMonth,timeGridWeek,timeGridDay,listWeek",
         }}
-        // events={events}
+        events={{
+          googleCalendarId: calendarID,
+          className: "gcal-event",
+        }}
+        eventClick={(info) => {
+          info.jsEvent.preventDefault();
+          console.log(info);
+        }}
       />
-      <MuiList>
-        {events !== undefined &&
-          events.map((event: any) => (
-            <ListItem>
-              <ListItemText primary={event.summary} />
-            </ListItem>
-          ))}
-      </MuiList>
-    </Box>
+    </List>
   );
 };
