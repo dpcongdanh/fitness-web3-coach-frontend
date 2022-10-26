f3a1879338a43adccd43d3406e4b79a1a8c99222
dpcongdanh <dpcongdanh@gmail.com>
2022-10-26 17:55:52 +0700

    WIP Posts and Trainers search

--- src/pages/posts/list.tsx
+++ src/pages/posts/list.tsx
@@ -1,8 +1,9 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import {
   useTranslate,
   // useMany,
   useList,
+  GetListResponse,
 } from "@pankod/refine-core";
 import {
   // useDataGrid,
@@ -16,25 +17,176 @@ import {
   Grid,
   CircularProgress,
   Typography,
+  Divider,
+  Paper,
+  InputBase,
+  IconButton,
+  MenuItem,
+  FormControl,
+  InputLabel,
+  // NativeSelect,
+  Select,
+  SelectChangeEvent,
 } from "@pankod/refine-mui";
 
-import { IPost } from "interfaces";
+import { Search } from "@mui/icons-material";
+
+import { IPost, ITrainer } from "interfaces";
 
 import { PostCard } from "../../components/post-card";
 
 export const PostList: React.FC = () => {
   const t = useTranslate();
 
-  const postListQueryResult = useList<IPost>({
+  const [isLoading, setIsLoading] = useState<Boolean>(false);
+
+  const [search, setSearch] = useState<string>("");
+
+  const [selectAuthor, setSelectAuthor] = useState<any>();
+
+  const [postsListResponse, setPostsListResponse] =
+    useState<GetListResponse<IPost>>();
+
+  const { refetch: refetchPosts } = useList<IPost>({
     resource: "posts",
     config: {
-      pagination: { current: 1, pageSize: 10 },
+      filters: [{ field: "title", operator: "contains", value: search }],
+    },
+    queryOptions: {
+      enabled: false,
+      onSuccess: (data) => {
+        setIsLoading(false);
+        if (data.total > 0) {
+          setPostsListResponse(data);
+        }
+      },
     },
   });
 
+  const { refetch: refetchPostsWithAuthor } = useList<IPost>({
+    resource: "posts",
+    config: {
+      filters: [
+        { field: "title", operator: "contains", value: search },
+        { field: "user_id", operator: "eq", value: selectAuthor },
+      ],
+    },
+    queryOptions: {
+      enabled: false,
+      onSuccess: (data) => {
+        setIsLoading(false);
+        if (data.total > 0) {
+          setPostsListResponse(data);
+        }
+      },
+    },
+  });
+
+  const trainerListQueryResult = useList<ITrainer>({
+    resource: "trainers",
+  });
+
+  useEffect(() => {
+    console.log(selectAuthor);
+    setIsLoading(true);
+    setPostsListResponse(undefined);
+    if (selectAuthor !== undefined && selectAuthor !== "")
+      refetchPostsWithAuthor();
+    else refetchPosts();
+  }, [refetchPosts, refetchPostsWithAuthor, search, selectAuthor]);
+
+  // const postListQueryResult = useList<IPost>({
+  //   resource: "posts",
+  //   config: {
+  //     pagination: { current: 1, pageSize: 10 },
+  //   },
+  // });
+
   return (
     <Box sx={{ flexGrow: 1 }}>
-      {postListQueryResult.isLoading ? (
+      <Paper
+        component="form"
+        sx={{ p: "2px 4px", display: "flex", alignItems: "center", width: 640 }}
+      >
+        <IconButton disabled type="button" sx={{ p: "10px" }} aria-label="menu">
+          <Search />
+        </IconButton>
+        <InputBase
+          sx={{ ml: 1, flex: 1 }}
+          placeholder="Search Posts"
+          value={search}
+          onChange={(
+            event: React.ChangeEvent<HTMLTextAreaElement | HTMLInputElement>
+          ) => {
+            setSearch(event.target.value);
+          }}
+          inputProps={{ "aria-label": "search posts" }}
+        />
+        {/* <IconButton
+          type="button"
+          sx={{ p: "10px" }}
+          aria-label="search"
+          onClick={(event: any) => {
+            console.log(event);
+          }}
+        >
+          <Search />
+        </IconButton> */}
+        {/* <Divider sx={{ height: 28, m: 0.5 }} orientation="vertical" /> */}
+        <Divider
+          sx={{
+            color: "text.secondary",
+            borderColor: "text.secondary",
+          }}
+          orientation="vertical"
+          flexItem
+        />
+        {/* <InputBase
+          sx={{ ml: 1, flex: 1 }}
+          placeholder="Search Posts"
+          value={search}
+          onChange={(
+            event: React.ChangeEvent<HTMLTextAreaElement | HTMLInputElement>
+          ) => {
+            setSearch(event.target.value);
+          }}
+          inputProps={{ "aria-label": "search posts" }}
+        /> */}
+        <FormControl sx={{ minWidth: 320 }}>
+          <InputLabel>Select Author</InputLabel>
+          <Select
+            sx={{ ml: 1, flex: 1 }}
+            variant="standard"
+            value={selectAuthor}
+            onChange={(
+              event: SelectChangeEvent<number>,
+              child: React.ReactNode
+            ) => {
+              setSelectAuthor(event.target.value);
+            }}
+            // label="Select Author"
+          >
+            <MenuItem value="">
+              <em>None</em>
+            </MenuItem>
+            {trainerListQueryResult.data !== undefined &&
+              trainerListQueryResult.data.total > 0 &&
+              trainerListQueryResult.data.data.map((row, index) => (
+                <MenuItem value={row.id}>
+                  {row.first_name + " " + row.last_name}
+                </MenuItem>
+              ))}
+            {/* <MenuItem value={10}>Ten</MenuItem>
+            <MenuItem value={20}>Twenty</MenuItem>
+            <MenuItem value={30}>Thirty</MenuItem> */}
+          </Select>
+        </FormControl>
+
+        {/* <IconButton color="primary" sx={{ p: '10px' }} aria-label="directions">
+        <DirectionsIcon />
+      </IconButton> */}
+      </Paper>
+      {isLoading ? (
         <Box
           sx={{
             display: "flex",
@@ -47,14 +199,13 @@ export const PostList: React.FC = () => {
           <CircularProgress />
           <Typography>{t("posts.loading")}</Typography>
         </Box>
-      ) : postListQueryResult.data !== undefined &&
-        postListQueryResult.data.total > 0 ? (
+      ) : postsListResponse !== undefined && postsListResponse.total > 0 ? (
         <Grid
           container
           spacing={{ xs: 2, md: 3 }}
           columns={{ xs: 3, sm: 6, md: 9, lg: 12 }}
         >
-          {postListQueryResult.data.data.map((row, index) => (
+          {postsListResponse.data.map((row, index) => (
             <Grid item xs={3} sm={3} md={3} lg={3} key={index}>
               <PostCard data={row}></PostCard>
             </Grid>
--- src/pages/trainers/list.tsx
+++ src/pages/trainers/list.tsx
@@ -1,8 +1,9 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import {
   useTranslate,
   // useMany,
   useList,
+  GetListResponse,
 } from "@pankod/refine-core";
 import {
   // useDataGrid,
@@ -16,7 +17,14 @@ import {
   Grid,
   CircularProgress,
   Typography,
+  TextField,
+  IconButton,
+  Paper,
+  InputBase,
+  Divider,
 } from "@pankod/refine-mui";
+
+import { Search } from "@mui/icons-material";
 
 import { ITrainer } from "interfaces";
 
@@ -27,20 +35,106 @@ export const TrainerList: React.FC = () => {
 
   // const { dataGridProps } = useDataGrid<ITrainer>();
 
-  const trainerListQueryResult = useList<ITrainer>({
+  const [isLoading, setIsLoading] = useState<Boolean>(false);
+
+  const [search, setSearch] = useState<string>("");
+  // const [trainerListQueryResult, setTrainerListQueryResult] = useState<
+  //   QueryObserverResult<GetListResponse<ITrainer>, HttpError>
+  // >([]);
+
+  const [trainerListResponse, setTrainerListResponse] =
+    useState<GetListResponse<ITrainer>>();
+
+  // const trainerListQueryResult = useList<ITrainer>({
+  //   resource: "trainers",
+  //   config: {
+  //     pagination: { current: 1, pageSize: 10 },
+  //   },
+  // });
+
+  const { refetch: refetchTrainers } = useList<ITrainer>({
     resource: "trainers",
     config: {
-      pagination: { current: 1, pageSize: 10 },
+      filters: [{ field: "first_name", operator: "contains", value: search }],
+    },
+    queryOptions: {
+      enabled: false,
+      onSuccess: (data) => {
+        setIsLoading(false);
+        if (data.total > 0) {
+          setTrainerListResponse(data);
+        }
+        // const postOptionGroup = data.data.map((item) =>
+        //     renderItem(item.title, "posts", item.id),
+        // );
+        // if (postOptionGroup.length > 0) {
+        //     setOptions([
+        //         {
+        //             label: renderTitle("Posts"),
+        //             options: postOptionGroup,
+        //         },
+        //     ]);
+        // }
+      },
     },
   });
 
-  console.log(trainerListQueryResult);
+  console.log(trainerListResponse);
 
-  console.log(trainerListQueryResult.data);
+  // console.log(trainerListQueryResult);
+
+  // console.log(trainerListQueryResult.data);
+
+  useEffect(() => {
+    setIsLoading(true);
+    setTrainerListResponse(undefined);
+    refetchTrainers();
+  }, [refetchTrainers, search]);
 
   return (
     <Box sx={{ flexGrow: 1 }}>
-      {trainerListQueryResult.isLoading ? (
+      <Paper
+        component="form"
+        sx={{ p: "2px 4px", display: "flex", alignItems: "center", width: 400 }}
+      >
+        {/* <IconButton sx={{ p: '10px' }} aria-label="menu">
+        <MenuIcon />
+      </IconButton> */}
+        <InputBase
+          sx={{ ml: 1, flex: 1 }}
+          placeholder="Search Trainers"
+          value={search}
+          onChange={(
+            event: React.ChangeEvent<HTMLTextAreaElement | HTMLInputElement>
+          ) => {
+            setSearch(event.target.value);
+          }}
+          inputProps={{ "aria-label": "search trainers" }}
+        />
+        <IconButton
+          type="button"
+          sx={{ p: "10px" }}
+          aria-label="search"
+          onClick={(event: any) => {
+            console.log(event);
+          }}
+        >
+          <Search />
+        </IconButton>
+        {/* <Divider sx={{ height: 28, m: 0.5 }} orientation="vertical" /> */}
+        <Divider
+          sx={{
+            color: "text.secondary",
+            borderColor: "text.secondary",
+          }}
+          orientation="vertical"
+          flexItem
+        />
+        {/* <IconButton color="primary" sx={{ p: '10px' }} aria-label="directions">
+        <DirectionsIcon />
+      </IconButton> */}
+      </Paper>
+      {isLoading ? (
         <Box
           sx={{
             display: "flex",
@@ -53,14 +147,13 @@ export const TrainerList: React.FC = () => {
           <CircularProgress />
           <Typography>{t("trainers.loading")}</Typography>
         </Box>
-      ) : trainerListQueryResult.data !== undefined &&
-        trainerListQueryResult.data.total > 0 ? (
+      ) : trainerListResponse !== undefined && trainerListResponse.total > 0 ? (
         <Grid
           container
           spacing={{ xs: 2, md: 3 }}
           columns={{ xs: 3, sm: 6, md: 9, lg: 12 }}
         >
-          {trainerListQueryResult.data.data.map((row, index) => (
+          {trainerListResponse.data.map((row, index) => (
             <Grid item xs={3} sm={3} md={3} lg={3} key={index}>
               <TrainerCard data={row}></TrainerCard>
             </Grid>
@@ -77,26 +170,91 @@ export const TrainerList: React.FC = () => {
         >
           No Trainers Found
         </Box>
-        // <Box sx={{ display: "flex", lineHeight: "calc(100vh - 112px)" }}>
-        //   No Doctors Found
-        // </Box>
       )}
     </Box>
-    // <Box sx={{ flexGrow: 1 }}>
-    //   <Grid
-    //     container
-    //     spacing={{ xs: 2, md: 3 }}
-    //     columns={{ xs: 3, sm: 6, md: 9, lg: 12 }}
-    //   >
-    //     {!trainerListQueryResult.isLoading &&
-    //       trainerListQueryResult.data !== undefined &&
-    //       trainerListQueryResult.data.data.map((row, index) => (
-    //         <Grid item xs={3} sm={3} md={3} lg={3} key={index}>
-    //           <TrainerCard data={row}></TrainerCard>
-    //         </Grid>
-    //       ))}
-    //   </Grid>
-    //   {/* <DataGrid {...dataGridProps} columns={columns} autoHeight /> */}
-    // </Box>
   );
+
+  // return (
+  //   <Box sx={{ flexGrow: 1 }}>
+  //     <Paper
+  //       component="form"
+  //       sx={{ p: "2px 4px", display: "flex", alignItems: "center", width: 400 }}
+  //     >
+  //       {/* <IconButton sx={{ p: '10px' }} aria-label="menu">
+  //       <MenuIcon />
+  //     </IconButton> */}
+  //       <InputBase
+  //         sx={{ ml: 1, flex: 1 }}
+  //         placeholder="Search Trainers"
+  //         value={search}
+  //         onChange={(
+  //           event: React.ChangeEvent<HTMLTextAreaElement | HTMLInputElement>
+  //         ) => {
+  //           setSearch(event.target.value);
+  //         }}
+  //         inputProps={{ "aria-label": "search trainers" }}
+  //       />
+  //       <IconButton
+  //         type="button"
+  //         sx={{ p: "10px" }}
+  //         aria-label="search"
+  //         onClick={(event: any) => {
+  //           console.log(event);
+  //         }}
+  //       >
+  //         <Search />
+  //       </IconButton>
+  //       {/* <Divider sx={{ height: 28, m: 0.5 }} orientation="vertical" /> */}
+  //       <Divider
+  //         sx={{
+  //           color: "text.secondary",
+  //           borderColor: "text.secondary",
+  //         }}
+  //         orientation="vertical"
+  //         flexItem
+  //       />
+  //       {/* <IconButton color="primary" sx={{ p: '10px' }} aria-label="directions">
+  //       <DirectionsIcon />
+  //     </IconButton> */}
+  //     </Paper>
+  //     {isLoading ? (
+  //       <Box
+  //         sx={{
+  //           display: "flex",
+  //           justifyContent: "center",
+  //           alignItems: "center",
+  //           flexDirection: "column",
+  //           height: "calc(100vh - 112px)",
+  //         }}
+  //       >
+  //         <CircularProgress />
+  //         <Typography>{t("trainers.loading")}</Typography>
+  //       </Box>
+  //     ) : trainerListQueryResult.data !== undefined &&
+  //       trainerListQueryResult.data.total > 0 ? (
+  //       <Grid
+  //         container
+  //         spacing={{ xs: 2, md: 3 }}
+  //         columns={{ xs: 3, sm: 6, md: 9, lg: 12 }}
+  //       >
+  //         {trainerListQueryResult.data.data.map((row, index) => (
+  //           <Grid item xs={3} sm={3} md={3} lg={3} key={index}>
+  //             <TrainerCard data={row}></TrainerCard>
+  //           </Grid>
+  //         ))}
+  //       </Grid>
+  //     ) : (
+  //       <Box
+  //         sx={{
+  //           display: "flex",
+  //           justifyContent: "center",
+  //           alignItems: "center",
+  //           height: "calc(100vh - 112px)",
+  //         }}
+  //       >
+  //         No Trainers Found
+  //       </Box>
+  //     )}
+  //   </Box>
+  // );
 };
